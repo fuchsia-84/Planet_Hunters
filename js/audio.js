0af5ad6d68ed385f551bb5bb08ff3862948cefd4
ふーしゃ / Fuchsia <46129202+fuchsia-84@users.noreply.github.com>
2023-08-07 23:16:11 +0900

    add bgm and sound on/off button to top section.

---
+++ js/audio.js
@@ -0,0 +1,19 @@
+let el = document.getElementById("bgm");
+var $audio_button = $('#audio_control button');
+function enableMute() {
+    el.muted = true;
+}
+
+function disableMute() {
+    el.muted = false;
+}
+
+$(function(){
+    $audio_button.click(function(){
+    $audio_button.toggleClass('active');
+    
+    if($this.hasClass('active') ) {
+        $this.css('display', 'none');
+    }
+  });
+});
